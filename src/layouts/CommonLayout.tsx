65abd545c855fca287f993c5449119fed07dec1f
Mahmoud Abu Sultan <mahmoodkns@hotmail.com>
2024-12-19 20:59:26 +0200

    fix:layout problems

--- src/layouts/CommonLayout.tsx
+++ src/layouts/CommonLayout.tsx
@@ -1,11 +1,21 @@
+import { cn } from "@/lib/utils";
 import { Navbar } from "../components";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export function CommonLayout() {
+  const pathname = useLocation().pathname;
+  console.log(pathname);
   return (
     <div className="flex flex-col">
       <Navbar />
-      <main className="mt-16 flex flex-col  justify-center items-center gap-10">
+      <main
+        className={cn(
+          "mt-16 flex flex-col  justify-center items-center  gap-10",
+          {
+            "mt-36": pathname == "/sign-in" || pathname == "/sign-up",
+          }
+        )}
+      >
         <Outlet />
       </main>
     </div>
